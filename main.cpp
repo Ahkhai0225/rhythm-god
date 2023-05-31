0227e89fa6ec50993f0a90c66dbf04c01a6b55c2
Ahkhai0225 <stargalaxy0225@gmail.com>
2023-05-31 07:25:20 +0000

    difficulty menu

--- main.cpp
+++ main.cpp
@@ -6,59 +6,87 @@
 AnalogIn pot1(p19);
 DigitalIn joystickCenter(p14);
 C12832 lcd(p5, p7, p6, p8, p11);
-BusIn joy(p15, p12);
+BusIn joy(p13, p16);
 BusOut leds(LED1, LED2, LED3, LED4);
+Serial pc(USBTX, USBRX);
 
 int main() {
   int i = 0;
   int dimX = 120;
-  int dimY = 28;
-  int x = 0, y = 0;
   lcd.cls();
   lcd.locate(0, 3);
-  lcd.printf("welcome to rythm-god game");
+  lcd.printf("welcome to rhythm-god game");
   ThisThread::sleep_for(2 * 100);
   int flag = 0;
-  int j = 0;
-  int cst = 4;
-  int fDimX = dimX - cst;
-  int fDimY = dimY - cst;
   int score = 0;
-
+  int difficulty = 0; // 0 - Easy, 1 - Medium, 2 - Hard
+  lcd.cls();
   while (1) {
     // CONDITION FOR CHECKING FOR THE center joystick
     if (joystickCenter) {
-      leds = 0xf;
       flag = 100;
       musicplayer speaker(p26);
       speaker.selectSong(1);
+      // Perform an action based on the selected difficulty
+      if (difficulty == 0) {
+        // Easy selected
+      } else if (difficulty == 1) {
+        // Medium selected
+      } else if (difficulty == 2) {
+        // Hard selected
+      }
     } else {
       if (flag == 0) {
         leds = joy;
-        // moving the snake up
-        if (joy == 0x1) {
-          j = j - cst;
-          if (j < 0)
-            j = dimY;
+        // moving arrow right
+        if (joy == 0x2) {
+          pc.printf("Hello");
+          // Update difficulty based on the arrow's position
+          if (i == 96) {
+            i = 0;
+            difficulty = 0; // Easy
+          } else if (i == 0) {
+            i = 42;
+            difficulty = 1; // Medium
+          } else if (i == 42) {
+            i = 96;
+            difficulty = 2; // Hard
+          }
+          lcd.cls();
         }
-        // moving the snake down
-        else if (joy == 0x2) {
-          j = (j + cst) % dimY;
+        // moving arrow left
+        else if (joy == 0x1) {
+          // Update difficulty based on the arrow's position
+          pc.printf("Hi");
+          if (i == 0) {
+            i = 96;
+            difficulty = 2; // Hard
+          } else if (i == 96) {
+            i = 42;
+            difficulty = 1; // Medium
+          } else if (i == 42) {
+            i = 0;
+            difficulty = 0; // Easy
+          }
+          lcd.cls();
         }
       }
     }
     if (flag == 0) {
-      lcd.cls();
-      lcd.locate(i, j);
+      lcd.locate(i, 0);
       lcd.printf("->");
-      lcd.locate(3, 0);
+      lcd.locate(10, 0);
       lcd.printf("Easy");
-      lcd.locate(15, 0);
+
+      lcd.locate(52, 0);
       lcd.printf("Medium");
-      lcd.locate(30, 0);
+
+      lcd.locate(106, 0);
       lcd.printf("Hard");
+
       lcd.locate(0, 22);
       lcd.printf("%d", score);
+      ThisThread::sleep_for(2 * 10);
     }
   }
 }
